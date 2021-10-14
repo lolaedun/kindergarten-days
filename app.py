2aace353262ade5f14e4bcc14772807355650383
Lola <mledun@hotmail.com>
2021-10-14 18:49:22 +0000

    add styling to not collapsed accordions

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ if os.path.exists("env.py"):
     import env
 
 
-
 app = Flask(__name__)
-
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -20,19 +17,18 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_activities")
 def get_activities():
     activities = list(mongo.db.activities.find())
     return render_template("activities.html", activities=activities)
 
 
-@app.route("/add_activity", methods=["GET", "POST"])
+@app.route("/add/activity", methods=["GET", "POST"])
 def add_activity():
     if request.method == "POST":
         activity = {
-            "month": request.form.getlist("month"),
+            "month": request.form.get("month"),
             "theme": request.form.get("theme"),
-            "letter_of_week": request.form.get("letter_of_week"),
+            "letter_of_week": request.form.get("letter-of-week"),
             "book": request.form.get("book"),
             "book_description": request.form.get("book_description"),
             "craft": request.form.get("craft"),
@@ -40,15 +36,13 @@ def add_activity():
             "game": request.form.get("game"),
             "game_description": request.form.get("game_description"),
             "watercolour": request.form.get("watercolour"),
-            "watercolour_description": request.form.get("watercolour_description")
-            
+            "watercolour_description": request.form.get("watercolour_description")          
         }
         mongo.db.activities.insert_one(activity)
         flash("Activity Successfully Added")
         return redirect(url_for("get_activities"))
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("add_activity.html", categories=categories)
-
+    return render_template("pages/add-activity.html", categories=categories)
 
 
 if __name__ == "__main__":
