12c32c90471d0b9dc6e7b3492ea49bbb4fcaa4a1
Lola <mledun@hotmail.com>
2021-11-09 20:26:59 +0000

    remove script conflict for hamburger and accordion

--- app.py
+++ app.py
@@ -122,8 +122,7 @@ def add_activity():
             "game": request.form.get("game"),
             "game_description": request.form.get("game_description"),
             "watercolour": request.form.get("watercolour"),
-            "watercolour_description": request.form.get("watercolour_description") 
-               
+            "watercolour_description": request.form.get("watercolour_description")   
         }
         mongo.db.activities.insert_one(activity)
         flash("Activity Successfully Added")
@@ -146,7 +145,7 @@ def edit_activity(activity_id):
             "game": request.form.get("game"),
             "game_description": request.form.get("game_description"),
             "watercolour": request.form.get("watercolour"),
-            "watercolour_description": request.form.get("watercolour_description")     
+            "watercolour_description": request.form.get("watercolour_description")
         }
         mongo.db.activities.update({"_id": ObjectId(activity_id)}, submit)
         flash("Activity Successfully Updated")
