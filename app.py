9913e48bbbf7b502c7493bd5915670ccdcbd2b36
Lola <mledun@hotmail.com>
2021-11-13 16:04:09 +0000

    add important dates to accordion

--- app.py
+++ app.py
@@ -124,7 +124,9 @@ def add_activity():
             "game": request.form.get("game"),
             "game_description": request.form.get("game_description"),
             "watercolour": request.form.get("watercolour"),
-            "watercolour_description": request.form.get("watercolour_description")
+            "watercolour_description": request.form.get("watercolour_description"),
+            "date": request.form.get("date"),
+            "date_description": request.form.get("date_description")
         }
         mongo.db.activities.insert_one(activity)
         flash("Activity Successfully Added")
@@ -147,7 +149,9 @@ def edit_activity(activity_id):
             "game": request.form.get("game"),
             "game_description": request.form.get("game_description"),
             "watercolour": request.form.get("watercolour"),
-            "watercolour_description": request.form.get("watercolour_description")
+            "watercolour_description": request.form.get("watercolour_description"),
+            "date": request.form.get("date"),
+            "date_description": request.form.get("date_description")
         }
         mongo.db.activities.update({"_id": ObjectId(activity_id)}, submit)
         flash("{} Activity Successfully Updated".format(request.form.get("theme")))
