06c3482b10dbdc259714ebcd69abb24b4239fdee
Lola <mledun@hotmail.com>
2021-11-30 18:07:28 +0000

    add script tidy up

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 
 // For disabling form submissions if there are invalid fields
+'use strict';
 (function() {
-  'use strict';
   window.addEventListener('load', function() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
+    const forms = document.querySelectorAll('needs-validation');
     // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
+    forms.filter.call((form) => {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
@@ -15,5 +15,5 @@
         form.classList.add('was-validated');
       }, false);
     });
-  }, false);
+  });
 })();
